61ab3136f3f19fb0ce157a3a6e9255eecfb92fed
chawarat-iam <chawarat.iam@gmail.com>
2019-11-24 14:55:17 +0700

    init

---
+++ app/src/main/java/com/ex07/carinformationcatalogproject/Details/BMW.kt
@@ -0,0 +1,13 @@
+package com.ex07.carinformationcatalogproject.Details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ex07.carinformationcatalogproject.R
+
+class BMW : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmw)
+    }
+}
